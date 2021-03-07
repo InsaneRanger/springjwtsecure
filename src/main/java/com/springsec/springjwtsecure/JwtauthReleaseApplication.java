c8cdc92061cc7208f08a37fb122980eb5533e4a9
Insane <valeev.timur.v@gmail.com>
2021-03-07 13:31:45 +0300

    Initial commit

---
+++ src/main/java/com/springsec/springjwtsecure/JwtauthReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.springsec.springjwtsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtauthReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springsec/springjwtsecure/JwtauthReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsec.springjwtsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
