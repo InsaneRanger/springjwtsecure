0767c83d577765b12e0f8715efa19921c48b2e34
Insane <valeev.timur.v@gmail.com>
2021-03-07 18:43:01 +0300

    Add logic JWT

---
+++ src/main/java/com/springsec/springjwtsecure/security/jwt/JWTTokenFilter.java
@@ -0,0 +1,44 @@
+package com.springsec.springjwtsecure.security.jwt;
+
+
+
+
+/*
+ * @author
+ * @version
+ * @return
+ */
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class JWTTokenFilter extends GenericFilterBean {
+
+    private final JWTTokenProvider jwtTokenProvider;
+
+    public JWTTokenFilter(JWTTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) servletRequest);
+
+        if(token!=null&& jwtTokenProvider.validateToken(token)){
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            if (authentication!=null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+    }
+}
